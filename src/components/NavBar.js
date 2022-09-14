07857c18c1d1d6912b8e53898be499d5bef2a4be
R-Aranda <rupertaranda@gmail.com>
2022-09-14 18:34:57 -0400

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import SideBar from "./SideBar";
+
+const NavBar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleClick = () => {
+    setNav(!nav);
+  };
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-slate-600 text-gray-300">
+      <div>
+        <p>RA</p>
+      </div>
+      <ul className="hidden md:flex">
+        <li>Home</li>
+        <li>About</li>
+        <li>Skills</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-slate-600 flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">About</li>
+        <li className="py-6 text-4xl">Skills</li>
+        <li className="py-6 text-4xl">Projects</li>
+        <li className="py-6 text-4xl">Contact</li>
+      </ul>
+      <SideBar />
+    </div>
+  );
+};
+
+export default NavBar;
